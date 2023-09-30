6e4961f388cf00d782c4b241716992f85d826078
faultypine <gclark1013@gmail.com>
2023-09-30 00:07:00 -0400

    save rpc progress

---
+++ src/tiny_rpc.h
@@ -0,0 +1,123 @@
+#ifndef TINY_RPC_H
+#define TINY_RPC_H
+
+#include "pch.h"
+
+// thanks Live++
+
+#define TINY_CONCATENATE_HELPER_HELPER(_a, _b)			_a##_b
+#define TINY_CONCATENATE_HELPER(_a, _b)					TINY_CONCATENATE_HELPER_HELPER(_a, _b)
+#define TINY_CONCATENATE(_a, _b)							TINY_CONCATENATE_HELPER(_a, _b)
+
+// Generates a unique identifier inside a translation unit.
+#define TINY_IDENTIFIER(_identifier)						TINY_CONCATENATE(_identifier, __LINE__)
+
+#if defined(__clang__)
+#	define TINY_HOOK_(_section, _function, ...)																												\
+		extern void (* const TINY_IDENTIFIER(TINY_CONCATENATE(tiny_hook_function, _function)))(__VA_ARGS__) __attribute__((section(_section)));				\
+		extern void (* const TINY_IDENTIFIER(TINY_CONCATENATE(tiny_hook_function, _function)))(__VA_ARGS__) __attribute__((section(_section))) = &_function
+#elif defined(_MSC_VER)
+#	define TINY_HOOK_(_section, _function, ...)																																				\
+		__pragma(section(_section, read)) __declspec(allocate(_section)) extern void (* const TINY_IDENTIFIER(TINY_CONCATENATE(tiny_hook_function, _function)))(__VA_ARGS__) = &_function
+#else
+#	error("Unknown compiler");
+#endif
+
+namespace tiny_hooks
+{
+    typedef struct TinyHookId { char unused; } TinyHookId;
+}
+
+
+#define TINY_HOOK_SECTION						".tiny_hooks"
+
+// Registers a pre-compile hook.
+#define TINY_HOOK(_function)					TINY_HOOK_(TINY_HOOK_SECTION, _function, tiny_hooks::TinyHookId, const void* const data, unsigned int size)
+
+
+struct Function
+{
+    enum VarType
+    {
+        VAR_VOID, VAR_BOOL, VAR_INT, VAR_FLOAT, VAR_STRING,
+    };
+    enum CallType
+    {
+        CALL_CDECL, CALL_FASTCALL, CALL_STDCALL, CALL_THISCALL,
+    };
+    typedef std::vector<VarType> ParamVec;
+    std::string name;
+    void* proc;
+    u32 serialID;
+    VarType returnType;
+    ParamVec paramTypes;
+    CallType callType;
+};
+typedef std::vector<Function> FunctionVec;
+FunctionVec g_Functions;
+
+
+
+
+/*
+non-float returns stored in eax
+eight-byte structures returned in edx and eax as a pair
+flaots/doubles returned through FPU in ST0
+ret values for user-defined types have addresses pushed onto stack last, but they will also be returned in eax
+
+__cdecl -- caller cleans up the stack. Caller is responsible for popping it's own arguments off stack after call completes.
+This is required for variable argument functions b/c called function might not have the info to pop correct num of args (this is default convention for static & global funcs in C/C++)
+
+__stdcall -- the called function cleans up the stack. Standard for win32 api calls (more efficient in client code size)
+
+__fastcall -- ? tbd
+__thiscall -- ? tbd
+
+
+ ---- for setting up function "marking"... maybe look at live++ and their function hook stuff
+DbgHelp.dll exports UndecorateSymbolName() which unmangles an exported function signature
+__declspec(dllexport) marks a function for exporting
+
+https://en.wikipedia.org/wiki/X86_calling_conventions#Microsoft_x64_calling_convention
+https://learn.microsoft.com/en-us/cpp/build/prolog-and-epilog?view=msvc-170&redirectedfrom=MSDN
+
+*/
+extern "C"
+{
+    __declspec(dllimport) void CallCDecl(const void* args, u64 size, void* func);
+    typedef void(__cdecl* CallCDeclFunc)(const void* args, u64 size, void* func);
+    void CallStdcall(const void* args, u64 size, void* func);
+    float GetST0();
+    u32 GetEIP();
+}
+
+void my_printer(int x)
+{
+    std::cout << x << "YESSS\n";
+}
+
+void tiny_rpc_test_init()
+{
+    HINSTANCE hinstLib = LoadLibraryA("lib/tiny_MASM.dll");
+    CallCDeclFunc cdeclfunc;
+    // If the handle is valid, try to get the function address.
+    if (hinstLib != NULL)
+    {
+        cdeclfunc = (CallCDeclFunc)GetProcAddress(hinstLib, "CallCDecl");
+    }
+    std::cout << "tiny_rpc_test_init\n";
+    int* args = new int[1]; 
+    args[0] = 9;
+    u64 size = 4;
+    cdeclfunc(args, size, &my_printer);
+    //CallCDecl(args, size, &my_printer); 
+    //CallStdcall(0, 0, 0);
+    //float x = GetST0();
+    //DWORD eip = GetEIP();
+    //std::cout << x << eip;
+    std::cout << "rpc success\n";
+}
+
+
+
+#endif
---
+++ sync_masm.bat
@@ -0,0 +1,3 @@
+@echo off
+
+robocopy C:/Dev/tiny_MASM/x64/Debug/ C:/Dev/tiny_engine/lib/ tiny_MASM.dll
