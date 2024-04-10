398b4791be1289be1af21e249485a0fa792b393a
faultypine <gclark1013@gmail.com>
2024-04-09 21:45:19 -0400

    switch from FNV-1 hash to FNV-1a

--- engine/src/tiny_engine.cpp
+++ engine/src/tiny_engine.cpp
@@ -170,7 +170,8 @@ u64 HashBytesL(u8* data, u32 size)
 }
 u32 HashBytes(u8* data, u32 size)
 {
-    // FNV-1 hash
+    // FNV-1 hash -> doing the *PRIME, and THEN the XOR.
+    // FNV-1a hash -> doing the XOR, then the *PRIME
     // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
     constexpr u32 FNV_offset_basis = 0x811c9dc5;
     constexpr u32 FNV_prime = 0x01000193;
@@ -178,8 +179,8 @@ u32 HashBytes(u8* data, u32 size)
     for (u32 i = 0; i < size; i++)
     {
         u8 byte_of_data = data[i];
-        hash = hash * FNV_prime;
         hash = hash ^ byte_of_data;
+        hash = hash * FNV_prime;
     }
     return hash;
 }
